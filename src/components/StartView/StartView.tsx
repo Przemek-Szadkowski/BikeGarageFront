88177a0e98ea4fe8565d4f800145ead715937d34
Przemek-Szadkowski <przemoszadkowski@o2.pl>
2022-05-25 15:42:23 +0200

    Made new components from main view

--- src/components/StartView/StartView.tsx
+++ src/components/StartView/StartView.tsx
@@ -1,27 +1,16 @@
+import React from "react";
+import { Logo } from "../common/Logo/Logo";
+import { StartForm } from "../StartForm/StartForm";
+import { Footer } from "../Footer/Footer";
+
 import './StartView.css';
 
 export const StartView =  () => {
     return (
-        <>
             <main className="start-view">
-                <div className="main-logo">
-                    <img src="logonaczarnym.jpg" alt="Garage logo"/>
-                </div>
-                <div>
-                    <form className="main-form" action="">
-                        <label>
-                            Numer zlecenia: <br/>
-                            <input type="number"/>
-                        </label>
-                        <label>
-                            Hasło: <br/>
-                            <input type="text"/>
-                        </label>
-                        <button>Wyświetl</button>
-                    </form>
-                </div>
-                <footer className="main-footer"><a className="main-footer-link" href="mailto:przemoszadkowski@o2.pl">&copy; Przemysław Szadkowski</a></footer>
+                <Logo/>
+                <StartForm/>
+                <Footer/>
             </main>
-        </>
-    )
-}
+    );
+};
